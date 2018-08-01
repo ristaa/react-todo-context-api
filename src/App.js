19c134a7160f7d70315f536b5dac870d3b092922
ristaa <rista90@gmail.com>
2018-08-01 10:00:47 +0200

    First app upload

--- src/App.js
+++ src/App.js
@@ -2,18 +2,118 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ToDoContext = React.createContext();
+
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [
+        {
+          todo: "clean the house"
+        },
+        {
+          todo: "buy milk"
+        }
+      ],
+      todo: ""
+    };
+  }
+
+  createNewToDoItem = () => {
+    this.setState(({ list, todo }) => ({
+      list: [
+        ...list,
+        {
+          todo: todo
+        }
+      ],
+      todo: ""
+    }));
+  };
+
+  deleteItem = indexToDelete => {
+    this.setState(({ list }) => ({
+      list: list.filter((ToDoList, index) => index !== indexToDelete)
+    }));
+  };
+
+  handleKeyPress = e => {
+    if (e.target.value !== "") {
+      if (e.key === "Enter") {
+        this.createNewToDoItem();
+      }
+    }
+  };
+
+  handleInput = e => {
+    this.setState({
+      todo: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <ToDoContext.Provider
+        value={{ list: this.state.list, deleteItem: this.deleteItem }}
+      >
+        <div className="ToDo">
+          <h1 className="ToDo-Header">
+            Simple To-Do App using React Context API
+          </h1>
+          <div className="ToDo-Container">
+            <input
+              type="text"
+              value={this.state.todo}
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+            />
+
+            <button className="ToDo-Add" onClick={this.createNewToDoItem}>
+              ADD ITEM
+            </button>
+          </div>
+        </div>
+
+        {this.props.children}
+      </ToDoContext.Provider>
+    );
+  }
+}
+
+class ToDoItems extends React.Component {
+  render() {
+    return (
+      <ToDoContext.Consumer>
+        {({ list, deleteItem }) =>
+          list.map((item, key) => {
+            return (
+              <div key={key}>
+                {item.todo}
+                <button
+                  className="ToDoItem-Delete"
+                  onClick={deleteItem.bind(this, key)}
+                >
+                  REMOVE
+                </button>
+              </div>
+            );
+          })
+        }
+      </ToDoContext.Consumer>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+		  <ToDoList>
+			<ToDoItems />
+		  </ToDoList>
+		</div>
     );
   }
 }
